e1a0dcf8e9d1c7b0c4adeb58013cc84370a0e97f
janiervv <56762034+janiervv@users.noreply.github.com>
2020-02-19 18:16:32 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import'./App.css';
-import Todolist from './Todolist';
+import './App.css'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact';
 
 function App() {
   return (
   <div className="App"> 
-  <header className ="App-header">TodoList</header>
-  <br></br>
-  <Todolist/>
+  <div class="App-header"><p>Welcome to React router</p></div>
+<div className="Router">
+    <BrowserRouter>
+        <div>
+
+        <Link to="/">Home</Link >{' '}
+        <Link to="/about">About</Link>{' '}
+        <Link to="/contact">Contact</Link>{' '}
+
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+</div>
   </div>
   );
 }
 
 export default App;
-
-
